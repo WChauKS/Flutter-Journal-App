78069a4cedc588f38acf805295923fc5c209bc4b
halsam <wchauks@gmail.com>
2020-08-02 22:48:12 -0400

    removed hard coded sql crud and import using dart io

--- lib/db/database_manager.dart
+++ lib/db/database_manager.dart
@@ -1,12 +1,14 @@
 import 'package:sqflite/sqflite.dart';
 import 'journal_entry_dto.dart';
+import 'package:flutter/services.dart';
 import '../models/entry.dart';
+
+const SQL_CREATE_SCHEMA = 'assets/sql_create_schema.txt';
+const SQL_INSERT = 'assets/sql_insert.txt';
+const SQL_SELECT = 'assets/sql_select.txt';
 
 class DatabaseManager {
   static const String DATABASE_FILENAME = 'journal.sqlite3.db';
-  static const String SQL_CREATE_SCHEMA = 'CREATE TABLE IF NOT EXISTS journal_entries(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, body TEXT NOT NULL, rating TEXT NOT NULL, date TEXT NOT NULL);';
-  static const String SQL_INSERT = 'INSERT INTO journal_entries(title, body, rating, date) VALUES(?, ?, ?, ?);';
-  static const String SQL_SELECT = 'SELECT * FROM journal_entries;';
 
   static DatabaseManager _instance;
 
@@ -20,10 +22,11 @@ class DatabaseManager {
   }
 
   static Future initialize() async {
+    String create = await rootBundle.loadString(SQL_CREATE_SCHEMA);
     final db = await openDatabase(DATABASE_FILENAME,
       version: 1,
       onCreate: (Database db, int version) async {
-        createTables(db, SQL_CREATE_SCHEMA);
+        createTables(db, create);
       }
     );
     _instance = DatabaseManager._(database: db);
@@ -35,14 +38,16 @@ class DatabaseManager {
 
   void saveJournalEntry({JournalEntryDTO dto}) {
     db.transaction( (txn) async{
-      await txn.rawInsert(SQL_INSERT,
+      String insert = await rootBundle.loadString(SQL_INSERT);
+      await txn.rawInsert(insert,
         [dto.title, dto.body, dto.rating, dto.date]
       );
     });
   }
 
   Future<List<Entry>> journalEntries() async {
-    final journalRecords = await db.rawQuery(SQL_SELECT);
+    String select = await rootBundle.loadString(SQL_SELECT);
+    final journalRecords = await db.rawQuery(select);
     final entries = journalRecords.map( (record) {
       return Entry(
         title: record['title'],
